d7a9c16298a43ac4d3ff6361d48ae56bd0ad5386
Alex <n_alex2001@mail.ru>
2019-12-06 14:21:22 +0300

    Выполнено

--- lesson01/task3.py
+++ lesson01/task3.py
@@ -1,3 +1,40 @@
+
+#Задача№1
+#Найти сумму и произведение цифр трехзначного числа, которое вводит пользователь
+#Ссылка:
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=lesson01_task01#R7Zndb5swEMD%2FGqSt0iYwHyGPI8k2Ta3UqQ9rH93gAZPByDEN7K%2BfDTYfdVuxrZGJ1BfHPt9h%2B%2B5%2BNjiWu8nrLxSW6RWJEbaAHdeWu7UAcGw35D9C0nQSP7Q7QUKzWCoNgpvsN1KWUlplMTpMFBkhmGXlVLgnRYH2bCKDlJLjVO0nwdNRS5ggTXCzh1iX%2FshilnbSEKwG%2BVeUJaka2QnWXU8OlbJcySGFMTmORO7OcjeUENbV8nqDsHCe8ktn9%2FmZ3n5iFBVsjsHtpb%2BO%2FSb9trk6sm363b2l8IOMzoE1asEo5uuXTUJZShJSQLwbpBElVREj8VSbtwadS0JKLnS48BdirJHBhBUjXJSyHMteVGfsVph%2F9GXrbtSzreWT20YjG908xeSeXb4UHUhF9%2BiFNas0gjRB7AU90AeJZzciOWK04XYUYciyh%2Bk8oEyzpNcbIsErMhh%2FERj53AeIKzmSxf2w3ooysi3urXCl6ryM2nKnRxNjToqI2jHNGLopYeuaI4d1GhM5HKIM1S97WPeINHBVpkvUPdk8Dtw4SiUdMaP0Xt2H63NJbu5g2oyMRPNu3DeYta0TQAFmQuGahAI8DUVbRkAiIErPAgHmy4juKa8loqZp%2BlJTlGELlJIDWzV5aStlXu9MOu50ErejCfhPTAAEMBfUFfeHsp1Qb9kN70xJ5lg9Tl9%2BhJSiygMMMUaYJBTm%2FFElohn3MKKP%2B66HDgMbgGPP3AGCU%2B0A6i1i%2BVvAK6LszkTZM4myq6EcOwI8UbWzgr2rLZ%2FT5W%2FfaxGcxsdAWofG09rRnLLQtF7AyebNxME3iYM3BwdnkTj0aW4OB%2FCGw2wc%2FJk4BCZx8OfgABaJgzv3s%2Bd0OLhvOMzGIZiJw8okDoGGw6HKex4EGlEMRKFH3jQOHjCOg38uOLxiWv9vukrTa8I321Fww2lw%2B8tc9YgOI2n1KG79NP49lCuNg7zCbATCRQwuFgiBb%2F5DeHUuECzgTHDmfjI4Rl%2BSHP2jQV1yOVYYzbkUa48RPrrdcXTe101%2B6JmmTD%2Bo24h8GkVhO7pPDHQoDd7XB6vTXdjz5vBPV3ccDP8Xurs%2F
+
+x = input("Введите трехзначное число\n")
+d1 = int(x[0])
+d2 = int(x[1])
+d3 = int(x[2])
+
+sum = d1+d2+d3
+mult = d1*d2*d3
+
+print("sum = ",sum)
+print("mult = ",mult)
+
+
+#Задача№2
+#Выполнить логические побитовые операции «И», «ИЛИ» и др. над числами 5 и 6.
+#Выполнить над числом 5 побитовый сдвиг вправо и влево на два знака.
+#Ссылка:
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=lesson01_task02#R7ZnLjpswFIafhmUrLsGQ5QxJW6mtGimLdroZeYIDtAYj40xIn7422AFiBnmkTskiG4fzH1%2FwOf4MOJYX5fVHCsv0K4kRtlw7ri1vZbmuY3sh%2FxHKqVX80G6FhGaxrNQJ2%2BwPUi2leshiVA0qMkIwy8qhuCNFgXZsoEFKyXFYbU%2FwcNQSJkgTtjuIdfV7FrO0VUM36PRPKEtSNbIDlq0nh6qynEmVwpgce5K3tryIEsLaq7yOEBbBU3Fp2314wXu%2BMYoKZtIgvdvvw99V4BGnAj83YPN5Fb%2BT2anYSU0YxXz%2B0iSUpSQhBcTrTr2n5FDESPRqc6ur84WQkosOF38hxk4ymfDACJdSlmPpRXXGfojm731pPfQ8q1r23BgnZRSMnnqNhPnQ93XNGku1a%2BcnJvVi2KRUkQPdoYlYqeUHaYLYRD3vnFxOBSI54vfD21GEIcueh%2FcB5fJMzvW6DPILmcRXJFT2%2BwzxQY5k8TgsV6K8ty0e5TBQ17y8b8q1vgow5oSJbB%2FTjKFtCZvQHDnkw1zK4RBlqJ6OsB4R2cBTrMstYiHNY8ebA6SW9lhT9f55DN3xGN7JWImyjaffxBNcVfSAM3f4lrc9xXhP8Qz3lMWce4qn8fCtRPQRFjFXG8H2xZULYC4WeFtyRSdjmOf%2FT4djG9IB3ooOdUM3PAzwWBji4c%2BJx2IcD0KHdATRdRIRzk6EowXlRsTkSjcgAsxJhD9ORH2JhL%2B%2BSiTOS30%2BJNwbEsZIAEMkgjmRABoSVKyjxyrN9mz8NSphTZyGlq2vjLlx8Uw%2F2N4OF%2F%2BGizEugSEuzqzfHIHGC0b7SVzwABd8tbgs3NlxGTmxuOEyiYEBL%2B6cuDj6R0hzZuU2J1TtKZ%2FbO7%2Fi1UGDyBN%2FJQPNs6X7po9arMb84g0uOkNWPFWl8tSjLr2bwWNPjNTn%2BmJVViksxSXPG8QYYZJQmPM%2B%2BHgZjxuil75N55iBbN%2F0cCF8Pdnc7P4qaHy9P1y89V8%3D
+
+Oper_and = 5 & 6
+Oper_or = 5 | 6
+Oper_xor = 5 ^ 6
+right_shift = 5 >> 2
+left_shift = 5 << 2
+
+print("Логическое И:", Oper_and)
+print("Логическое ИЛИ:", Oper_or)
+print("Логическое XOR:", Oper_xor)
+print("Сдвиг вправо на два бита:", right_shift)
+print("Сдвиг влево на два бита:", left_shift)
+
+
 #Задача№3
 #По введенным пользователем координатам двух точек
 #вывести уравнение прямой вида y = kx + b, проходящей через эти точки.
@@ -21,3 +58,19 @@ b = y1 - k*x1
 print("y = {0}*x + {1}".format(k,b))
 
 
+#Задача№5
+#Пользователь вводит две буквы. Определить, на каких местах алфавита они стоят,
+#и сколько между ними находится букв.
+#Ссылка:
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=lesson01_task05#R7Vjfb5swEP5rkNqHVsRAfjyONO0euqlaHrbuZXLDFVwZjIzTJPvrd4AdoKSRN6mjlfIQ4vt8B%2Fadv88Gx5un2xtJ8%2BSLiIA7xI22jnflEDJyvSn%2BlciuRoKpWwOxZJF2aoAl%2Bw0mUqNrFkHRcVRCcMXyLrgSWQYr1cGolGLTdXsUvPvUnMbQA5YryvvodxappEanZNLgn4HFiXnyaDyre1JqnPVMioRGYtOCvIXjzaUQqm6l2znwMnkmL3Xc9Su9%2B4FJyJRNwNNE%2BDfhV5kuv13f0Zlkt8XPC12dQu3MhCHC%2BWtTSJWIWGSULxo0lGKdRVDe1UWr8bkVIkdwhOATKLXTxaRrJRBKVMp1L2yZ%2BlGGXwbaum%2F1XG31nStjZ4xMyV0rqDTv231NWGWZuHp%2B5aReTZuGCrGWKziSK7P8qIxBHfHz98VFVoBIAceDcRI4Vey5Ow6ql2e892sqiA1dxL8oqL7vM%2BVr%2FSQH8zCrrmF9XVRXHOWY4zTCB4mtuGytEoyerxLSWxS4dvOyiWmjnAMXsaQpRuQgGY4b5Mu%2Bu6Yj3CRMwTKnVXI3KBPd1aAHDFLB9niN%2BjnVAZ7h2M6Ih7Y3DWVHxidp0XXsvlEZJideWfOKWPLqlTXwf3hFDvPqquKS62CWpxPTxmuomdZbBZzjzgWDsMK3JIX%2FVqTwDufwU0uV6nwGVT7H7yp7ga2mvFn6ZidNsdYU31JTgiE1xe%2FxIRcFU0xkv6qt2K1AV8joDO1LLjYgz87PEbrA36zPj261B9h3p0Pvu%2Ba14UQSmwOt7c47HvRE651Kal3SwPYsRYYsaXBM%2BMgL4SMfQPj2gjac8I16STmx5KieWbDEG5Ik48OnZVKdjUOb9%2FnuaWJ%2B1IEcdIjYIzZpWjIoeyjyfRk%2B7rcBnwxO1f67ZJVnrXr0oTh7IYel6rWLef7uBNCzfbn8h6yi2Xwlrfpa35q9xR8%3D
+
+ch1 = input("Введите первую букву:\n")
+ch2 = input("Введите вторую букву:\n")
+
+position_ch1 = ord(ch1.lower()) - 96
+position_ch2 = ord(ch2.lower()) - 96
+dif = abs(position_ch2 - position_ch1 - 1)
+
+print("Позиция первой буквы:",position_ch1)
+print("Позиция второй буквы:",position_ch2)
+print("Количество букв между:",dif)
